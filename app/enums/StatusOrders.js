ca31502ff29e46566fb1d2f9b65f9994cfb8fdc3
PhuongA1611 <tranthiphuong160611@gmail.com>
2023-05-20 18:27:52 +0700

    update

--- app/enums/StatusOrders.js
+++ app/enums/StatusOrders.js
@@ -3,10 +3,10 @@ const Enum = require('./Enum');
 module.exports = new Enum({
   PENDING: 'pending',
   PROCESSING: 'processing',
-  ON_HOLD: 'on-hold',
+  // ON_HOLD: 'on-hold',
   COMPLETED: 'completed',
   CANCELLED: 'cancelled',
-  REFUNDED: 'refunded',
-  FAILED: 'failed',
-  TRASH: 'trash',
+  // REFUNDED: 'refunded',
+  // FAILED: 'failed',
+  // TRASH: 'trash',
 });
--- app/http/middlewares/uploadFile.js
+++ app/http/middlewares/uploadFile.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
+  limits: { fileSize: 1024 * 1024 * 10 },
 });
 
 module.exports = upload;
